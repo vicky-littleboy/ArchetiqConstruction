57df2c74b407e400af14e89c4958a31c22a4ee8e
v.is.learning78 <v.is.learning78@gmail.com>
2025-01-28 16:26:12 +0000

    changes made on 28th jan

---
+++ script.js
@@ -0,0 +1,7 @@
+
+        // JavaScript to handle the click event on the menu button
+        document.getElementById('menuButton').addEventListener('click', function() {
+            const navMd = document.getElementById('navmd');
+            navMd.classList.toggle('active'); // Toggle the 'active' class
+        });
+//    javaScript to get input values from form
